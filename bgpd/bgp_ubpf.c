a524ad951017ba585dff676cc69e2ab0faab4786
twirtgen <thomas.wirtgen@uclouvain.be>
2022-04-05 14:30:52 +0200

    Intern custom_attr each to a new attr is added

--- bgpd/bgp_ubpf.c
+++ bgpd/bgp_ubpf.c
@@ -396,7 +396,7 @@ int add_attr__(context_t *ctx, uint8_t code, uint8_t flags, uint16_t length,
         assert(frr_attr->custom_attrs->refcount == 0);
     }
 
-            DL_SEARCH(frr_attr->custom_attrs->head_hash, rt_find, rt_attr, rte_attr_cmp);
+    DL_SEARCH(frr_attr->custom_attrs->head_hash, rt_find, rt_attr, rte_attr_cmp);
     if (rt_find) {
         DL_DELETE(frr_attr->custom_attrs->head_hash, rt_find);
         XFREE(MTYPE_UBPF_ATTR, rt_find);
@@ -404,6 +404,10 @@ int add_attr__(context_t *ctx, uint8_t code, uint8_t flags, uint16_t length,
     }
 
     DL_APPEND(frr_attr->custom_attrs->head_hash, rt_attr);
+
+    // we should intern custom_attr here since it has been
+    // modified by this function. It seems to be overkill...
+    frr_attr->custom_attrs = rte_attr_intern(frr_attr->custom_attrs);
     return 0;
 }
 
